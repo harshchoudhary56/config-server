4b8c516a1dd0e8c65675a660e90029e88eec66fb
harsh.choudhary <harsh.choudhary@freecharge.com>
2026-01-08 23:24:33 +0530

    refactor: update package structure and group ID

--- src/main/java/com/apple/inc/server/ConfigServerApplication.java
+++ src/main/java/com/apple/inc/server/ConfigServerApplication.java
@@ -1,4 +1,4 @@
-package com.school_management_system.config_server;
+package com.apple.inc.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
